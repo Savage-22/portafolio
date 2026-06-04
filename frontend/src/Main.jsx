5f9322d32d586a5fc875241d37cf739167a656e5
Jeik Pasquel <jeik11109@gmail.com>
2026-06-04 17:17:41 -0500

    feat & fix: Comentario el bug de prueba y actualización de proyecto rutero

--- frontend/src/Main.jsx
+++ frontend/src/Main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Rutas from './Routes.jsx'
 import { ThemeProvider } from './context/ThemeContext'
-import { NoExiste } from './ComponenteInventado';
+//import { NoExiste } from './ComponenteInventado';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
